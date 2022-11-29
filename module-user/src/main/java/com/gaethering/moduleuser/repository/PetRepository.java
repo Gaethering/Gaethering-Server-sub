71af29c68b8f65678c0dd7c743fd2403ca9a5ac2
naekang <rlawlsgh6306@gmail.com>
2022-11-29 14:14:40 +0900

    feat: PetRepository 작성

---
+++ module-user/src/main/java/com/gaethering/moduleuser/repository/PetRepository.java
@@ -0,0 +1,10 @@
+package com.gaethering.moduleuser.repository;
+
+import com.gaethering.moduleuser.domain.Pet;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PetRepository extends JpaRepository<Pet, Long> {
+
+}
