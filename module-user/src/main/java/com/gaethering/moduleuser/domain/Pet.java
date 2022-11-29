35b97e9b4a3cec3274eaba653dc255a11ae03517
naekang <rlawlsgh6306@gmail.com>
2022-11-29 13:29:00 +0900

    feat: Pet entity 구현

---
+++ module-user/src/main/java/com/gaethering/moduleuser/domain/Pet.java
@@ -0,0 +1,49 @@
+package com.gaethering.moduleuser.domain;
+
+import com.gaethering.moduleuser.type.Gender;
+import java.time.LocalDate;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Pet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "pet_id")
+    private Long id;
+
+    private String name;
+
+    private LocalDate birth;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    private String breed;
+
+    private float weight;
+
+    private boolean isNeutered;
+
+    private boolean isRepresentative;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    private String imageUrl;
+
+}
---
+++ module-user/src/main/java/com/gaethering/moduleuser/type/Gender.java
@@ -0,0 +1,5 @@
+package com.gaethering.moduleuser.type;
+
+public enum Gender {
+    MALE, FEMALE
+}
